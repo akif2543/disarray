4ef02b102a8c31fd80c4c14f88c6c929f958baaf
Akif Saifi <akifsaifi@gmail.com>
2020-06-01 13:53:02 -0400

    Initial setup and icon library

---
+++ frontend/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./icons/fa_library";
+
+const Root = (props) => <div></div>;
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<Root />, document.getElementById("root"));
+});
