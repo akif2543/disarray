0a56ba6eed818f6a4d4dc4e98099a5cace9d24c6
Akif Saifi <akifsaifi@gmail.com>
2020-07-07 10:28:22 -0400

    add angel list link to splash

--- frontend/components/splash/splash_bar.jsx
+++ frontend/components/splash/splash_bar.jsx
@@ -2,56 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SplashBar = ({ loggedIn, handleDemo, startLoading }) => (
+const SplashBar = ({ loggedIn, startLoading }) => (
   <nav className="splash-nav">
     <nav>
       <Link to="/">
         <img src={window.logoIconURL} className="splash-logo-icon" alt="" />
         <img src={window.logoURL} className="splash-logo" alt="" />
       </Link>
-      {/* <ul className="splash-nav-left">
-        <li>
-          <Link to="/@me" onClick={handleDemo}>
-            <p>Demo</p>
-          </Link>
-        </li> */}
-      {/* <li>
-          <p>Why Disarray?</p>
-        </li>
-        <li>
-          <p>Nitro</p>
-        </li>
-        <li>
-          <p>Jobs</p>
-        </li>
-        <li>
-          <p>
-            Developers <FontAwesomeIcon icon="angle-down" size="xs" />
-          </p>
-        </li>
-        <li>
-          <p>
-            Community <FontAwesomeIcon icon="angle-down" size="xs" />
-          </p>
-        </li>
-        <li>
-          <p>
-            Support <FontAwesomeIcon icon="angle-down" size="xs" />
-          </p>
-        </li> */}
-      {/* </ul> */}
     </nav>
     <ul className="splash-nav-right">
-      <a href="https://www.github.com/akif2543" className="splash-nav-link">
+      <a
+        href="https://www.github.com/akif2543"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="splash-nav-link"
+      >
         <FontAwesomeIcon icon={["fab", "github"]} className="splash-icon" />
       </a>
       <a
         href="https://www.linkedin.com/in/akifsaifi"
+        target="_blank"
+        rel="noopener noreferrer"
         className="splash-nav-link"
       >
         <FontAwesomeIcon icon={["fab", "linkedin"]} className="splash-icon" />
       </a>
-      <a href="https://www.instagram.com/discord" className="splash-nav-link">
+      <a
+        href="https://angel.co/u/akif-saifi"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="splash-nav-link"
+      >
         <FontAwesomeIcon icon={["fab", "angellist"]} className="splash-icon" />
       </a>
       <Link
