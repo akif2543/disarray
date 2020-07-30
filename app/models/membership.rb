94a98c8d60f7047756ab9449952fb7da897aa128
Akif Saifi <akifsaifi@gmail.com>
2020-07-29 23:56:20 -0400

    Yikes, fix scope syntax in model validation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,6 @@
 class Membership < ApplicationRecord
   validates :subscribeable_type, :subscribeable_id, presence: true
-  validates :member_id, presence: true, uniqueness: { scope: :subscribeable_type, :subscribeable_id, message: "Already a member" }
+  validates :member_id, presence: true, uniqueness: { scope: [:subscribeable_type, :subscribeable_id], message: "Already a member" }
 
   belongs_to :subscribeable, polymorphic: true, touch: true
   belongs_to :member, foreign_key: :member_id, class_name: :User, touch: true
