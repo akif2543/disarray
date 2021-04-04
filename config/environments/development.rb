5acb9bf8b890719d91246eee40803447de0dbded
Akif Saifi <akifsaifi@gmail.com>
2021-04-04 14:37:26 -0400

    remove credentials, active storage config, and aws references

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon_dev
+  config.active_storage.service = :local  # TO FORK: replace this with your own active storage service
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
